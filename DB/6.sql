0c171bf22264a3520e3be74d083a0bdaf52f7102
stepan41k <russia.zont@gmail.com>
2025-10-17 14:06:49 +0300

    synchronized with another labs

--- DB/6.sql
+++ DB/6.sql
@@ -14,40 +14,40 @@
             -- 4.1.1. Выведите фамилию и имена студента, и название команды, в которой он состоит.
                 SELECT s.lastname, s.firstname, c.command
                 FROM z5_student s
-                JOIN z5_command c ON s.idcommand = c.id;
+                INNER JOIN z5_command c ON s.idcommand = c.id;
             -- 4.1.2. Выведите фамилии студентов, которые выполняют проект по указанной теме (тема ваша, например, исследовательский).
                 SELECT s.lastname, s.firstname
                 FROM z5_student s
-                JOIN z5_project p ON s.idcommand = p.idcommand
-                WHERE p.project_type = 'Исследовательский';
+                INNER JOIN z5_project p ON s.idcommand = p.idcommand
+                WHERE p.project_type = 'исследовательский';
             -- 4.1.3. Выберите все проекты команды A&B.
                 SELECT p.projectname, p.about, p.startdate, p.enddate, p.status
                 FROM z5_project p
-                JOIN z5_command c ON p.idcommand = c.id
+                INNER JOIN z5_command c ON p.idcommand = c.id
                 WHERE c.command = 'A&B';
             -- 4.1.4. Найдите фамилии и имена тех студентов, которые работают над проектами, название которых начинается на указанную букву (например, буква «К») в период от начала года до текущей даты.
-                SELECT s.lastname, s.firstname
+                SELECT s.lastname, s.firstname, p.projectname
                 FROM z5_student s
-                JOIN z5_project p ON s.idcommand = p.idcommand
-                WHERE p.projectname LIKE 'К%' AND p.startdate >= DATE_TRUNC('year', CURRENT_DATE) AND p.startdate <= CURRENT_DATE;
+                INNER JOIN z5_project p ON s.idcommand = p.idcommand
+                WHERE p.projectname LIKE 'П%' AND p.startdate >= DATE_TRUNC('year', CURRENT_DATE) AND p.startdate <= CURRENT_DATE;
             -- 4.1.5. Перечислите студентов, котораые в команде под руководством Телиной Ирины Сергеевны.
                 SELECT s.lastname, s.firstname
                 FROM z5_student s
-                JOIN z5_mentor m ON s.idcommand = m.idcommand
+                INNER JOIN z5_mentor m ON s.idcommand = m.idcommand
                 WHERE m.lastname = 'Телина' AND m.firstname = 'Ирина Сергеевна';
 
         -- 4.2. Агрегатные функции в многотабличных запросах
             -- 4.2.1. Подсчитайте среднюю стоимость и количество выполняемых ей проектов каждой команды.
-                SELECT c.command, COUNT(p.id) AS number_of_projects, AVG(p.price) AS average_price
+                SELECT c.command, COUNT(p.id) AS number_of_projects, ROUND(AVG(p.price), 2) AS average_price
                 FROM z5_command c
-                JOIN z5_project p ON c.id = p.idcommand
+                INNER JOIN z5_project p ON c.id = p.idcommand
                 GROUP BY c.command;
             -- 4.2.2. * (having) Найдите количество работ над проектами, у которых название команды которых начинается на «Р»
                 SELECT c.command, COUNT(p.id) AS project_count
                 FROM z5_command c
-                JOIN z5_project p ON c.id = p.idcommand
+                INNER JOIN z5_project p ON c.id = p.idcommand
                 GROUP BY c.command
-                HAVING c.command LIKE 'Р%';
+                HAVING c.command LIKE 'R%';
             -- 4.2.3. Решите задачу с применением filter
                 SELECT c.command, COUNT(p.id) FILTER (WHERE p.project_type = 'коммерческий') AS commercial_project_count
                 FROM z5_command c
@@ -64,16 +64,31 @@
                 LEFT JOIN z5_student s ON g.groupname = s.groupname
                 WHERE s.id IS NULL;
             -- 4.3.3. Получите список групп, в которых нет ни одного студента.
-
+                SELECT g.groupname
+                FROM z5_grouplist AS g
+                LEFT JOIN z5_student s ON g.groupname = s.groupname
+                WHERE s.id IS NULL;
         -- 4.4. Множественные операции 
             -- 4.4.1. Выведите название команд, которые выполняют проекты, стоимость которых >10000 и <20000 (оператор UNION ALL и UNION). Результаты сравните.
-                SELECT c.command FROM z5_command c JOIN z5_project p ON c.id = p.idcommand WHERE p.price > 10000
+                SELECT c.command
+                FROM z5_command c
+                INNER JOIN z5_project p ON c.id = p.idcommand
+                WHERE p.price > 10000
                 UNION
-                SELECT c.command FROM z5_command c JOIN z5_project p ON c.id = p.idcommand WHERE p.price < 20000;
+                SELECT c.command
+                FROM z5_command c 
+                INNER JOIN z5_project p ON c.id = p.idcommand
+                WHERE p.price < 20000;
 
-                SELECT c.command FROM z5_command c JOIN z5_project p ON c.id = p.idcommand WHERE p.price > 10000
+                SELECT c.command
+                FROM z5_command c 
+                INNER JOIN z5_project p ON c.id = p.idcommand
+                WHERE p.price > 10000
                 UNION ALL
-                SELECT c.command FROM z5_command c JOIN z5_project p ON c.id = p.idcommand WHERE p.price < 20000;
+                SELECT c.command 
+                FROM z5_command c INNER ADD
+                JOIN z5_project p ON c.id = p.idcommand 
+                WHERE p.price < 20000;
             -- 4.4.2. Выбрать все записи о проектах, стоимость которых больше 1000р. и руководитель Цымбалюк Л.Н. (оператор пересечения).
                 SELECT id, projectname, about, startdate, enddate, status, price, idcommand, project_type
                 FROM z5_project
@@ -82,13 +97,16 @@
                 SELECT p.id, p.projectname, p.about, p.startdate, p.enddate, p.status, p.price, p.idcommand, p.project_type
                 FROM z5_project p
                 JOIN z5_mentor m ON p.idcommand = m.idcommand
-                WHERE m.lastname = 'Цымбалюк' AND m.firstname = 'Л.Н.';
+                WHERE m.lastname = 'Цымбалюк' AND m.firstname = 'Лариса Николаевна';
             -- 4.4.3. Придумайте и реализуйте пример разности.
-                -- Команды, у которых есть хоть какой-то проект
-                SELECT c.command FROM z5_command c JOIN z5_project p ON c.id = p.idcommand
+                SELECT c.command 
+                FROM z5_command c 
+                INNER JOIN z5_project p ON c.id = p.idcommand
                 EXCEPT
-                -- Команды, у которых есть проект типа 'внутренний'
-                SELECT c.command FROM z5_command c JOIN z5_project p ON c.id = p.idcommand WHERE p.project_type = 'внутренний';
+                SELECT c.command 
+                FROM z5_command c 
+                INNER JOIN z5_project p ON c.id = p.idcommand 
+                WHERE p.project_type = 'внутренний';
 
         -- 4.5. Использование подзапросов
             -- 4.5.1. Агрегатные функции в подзапросах: Выведите на экран все проекты, которые имеют стоимость, большую средней стоимости проектов.
@@ -102,15 +120,15 @@
             -- 4.5.3. Найдите все проекты, которые имеет стоимость такую же, как стоимость указанного проекта.
                 SELECT projectname, price
                 FROM z5_project
-                WHERE price = (SELECT price FROM z5_project WHERE projectname = 'Название конкретного проекта');
+                WHERE price = (SELECT price FROM z5_project WHERE projectname = 'Пентест веб-приложения');
             -- 4.5.4. (HAVING): Выведите количество проектов каждой команды, у которых фамилия руководителя содержит букву а.
                 SELECT c.command, COUNT(p.id) AS project_count
                 FROM z5_command c
                 JOIN z5_project p ON c.id = p.idcommand
-                WHERE c.id IN (SELECT idcommand FROM z5_mentor WHERE lastname LIKE '%а%')
-                GROUP BY c.command;
+                GROUP BY c.id, c.command
+                HAVING c.id IN (SELECT idcommand FROM z5_mentor WHERE lastname LIKE '%а%');
             -- 4.5.5. (Запрос с подзапросом после Select) – найдите название проектов и разницу с стоимости проекта и средней стоимостью всех проектов
-                SELECT projectname, price, price - (SELECT AVG(price) FROM z5_project) AS price_difference
+                SELECT projectname, price, ROUND(price - (SELECT AVG(price) FROM z5_project), 2) AS price_difference
                 FROM z5_project;
             -- 4.5.6. Перечислите команды, которые не работали ни с одним проектом типа ‘исследовательский;
                 SELECT command
@@ -187,19 +205,17 @@
                         descr TEXT
                     );
                 -- • Напишите запрос на выборку к generate_series(1,10) AS id с полями id*10, (id-1)*10, (id+1)*10, 'Event ' || id*10
+                    SELECT id, (id-1)*10 AS predid, (id+1)*10 AS postid, 'Event ' || id*10 AS descr
+                    FROM generate_series(1,10) AS id;
                 -- • Напишите запрос на вставку 10 записей в таблицу events, как результат предыдущего запроса
                     INSERT INTO z5_events (id, predid, postid, descr)
-                    SELECT
-                        id,
-                        (id-1)*10,
-                        (id+1)*10,
-                        'Event ' || id*10
+                    SELECT id, (id-1)*10, (id+1)*10, 'Event ' || id*10
                     FROM generate_series(1,10) AS id;
                 -- • В результате вы должны получить 
                 -- • Напишите рекурсивный запрос, который найдёт id события от события с id=10
                     WITH RECURSIVE event_chain AS (
                         SELECT id, predid, descr
-                        FROM events
+                        FROM z5_events
                         WHERE id = 10
 
                         UNION ALL
@@ -218,11 +234,11 @@
                 WITH RECURSIVE subordinates AS (
                     SELECT id, lastname, firstname, idcommand, level
                     FROM z5_student
-                    WHERE lastname = 'фамилия_руководителя' -- Задаем начального пользователя
+                    WHERE lastname = 'Соколов'
 
                     UNION ALL
 
-                    SELECT s.id, s.lastname, s.firstname, s.idcommand, s.level
+                    SELECT s.id, s.lastname, s.firsrfr tname, s.idcommand, s.level
                     FROM z5_student s
                     INNER JOIN subordinates sub ON s.idcommand = sub.id
                 )
@@ -258,21 +274,20 @@
                     );
 -- По каждой группе найдите количество выполненных задач (соответственно, предусмотрите, дополнение в базе данных – закрепление задачи на студентом)
     WITH student_age_groups AS (
-    SELECT
-        id,
-        CASE
-            WHEN (2024 - yearb) BETWEEN 7 AND 17 THEN 'начинающий'
-            WHEN (2024 - yearb) BETWEEN 18 AND 24 THEN 'продвинутый'
-            WHEN (2024 - yearb) BETWEEN 25 AND 35 THEN 'профессионал'
-            WHEN (2024 - yearb) >= 36 THEN 'эксперт'
-            ELSE 'не определено'
-        END AS age_group
-    FROM
-        student
-)
-SELECT sag.age_group, COUNT(st.id) AS completed_tasks_count
-FROM student_age_groups sag
-JOIN z5_student_task st ON sag.id = st.idstudent
-WHERE st.completed = TRUE
-GROUP BY sag.age_group
-ORDER BY sag.age_group;
+        SELECT
+            id,
+            CASE
+                WHEN (2024 - yearb) BETWEEN 7 AND 17 THEN 'начинающий'
+                WHEN (2024 - yearb) BETWEEN 18 AND 24 THEN 'продвинутый'
+                WHEN (2024 - yearb) BETWEEN 25 AND 35 THEN 'профессионал'
+                WHEN (2024 - yearb) >= 36 THEN 'эксперт'
+                ELSE 'не определено'
+            END AS age_group
+        FROM z5_student
+    )
+    SELECT sag.age_group, COUNT(st.id) AS completed_tasks_count
+    FROM student_age_groups sag
+    JOIN z5_student_task st ON sag.id = st.idstudent
+    WHERE st.completed = TRUE
+    GROUP BY sag.age_group
+    ORDER BY sag.age_group;
