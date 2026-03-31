791ae72223380bdfaded0cb1ccb99ad642741c4f
stepna41k <russia.zont@gmail.com>
2026-03-31 05:54:24 +0300

    struct: remoe Seminar.java

--- OOP/laba_1/src/main/java/unischedule/model/Seminar.java
+++
@@ -1,6 +0,0 @@
-package unischedule.model;
-
-public class Seminar extends Lesson {
-    public Seminar(String subject, Teacher t, StudentGroup g, Classroom c, TimeSlot ts) { super(subject, t, g, c, ts); }
-    @Override public String getLessonType() { return "Семинар"; }
-}
