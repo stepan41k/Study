ba5ab33d2c5d3a318a4ad77d4fbc350e32ca0dd7
stepna41k <russia.zont@gmail.com>
2026-03-31 05:58:06 +0300

    feat: update lesson model

--- OOP/laba_1/src/main/java/unischedule/model/Lesson.java
+++ OOP/laba_1/src/main/java/unischedule/model/Lesson.java
@@ -1,27 +1,34 @@
 package unischedule.model;
 
+import java.time.LocalDate;
+
 public abstract class Lesson {
+    private LocalDate date;
     private String subject;
     private Teacher teacher;
     private StudentGroup group;
     private Classroom classroom;
     private TimeSlot timeSlot;
+    private String comment = "";
 
-    public Lesson(String subject, Teacher teacher, StudentGroup group, Classroom classroom, TimeSlot timeSlot) {
+    public Lesson(String subject, Teacher teacher, StudentGroup group, Classroom classroom, TimeSlot timeSlot, LocalDate date) {
         this.subject = subject;
         this.teacher = teacher;
         this.group = group;
         this.classroom = classroom;
         this.timeSlot = timeSlot;
+        this.date = date;
     }
 
-    // Геттеры
+    public LocalDate getDate() { return date; }
     public String getSubject() { return subject; }
     public Teacher getTeacher() { return teacher; }
     public StudentGroup getGroup() { return group; }
     public Classroom getClassroom() { return classroom; }
     public TimeSlot getTimeSlot() { return timeSlot; }
-
-    // Полиморфизм: каждый подкласс вернет свой тип
+    public String getComment() { return comment; }
     public abstract String getLessonType();
+
+    public void setDate(LocalDate date) { this.date = date; }
+    public void setComment(String comment) { this.comment = comment; }
 }
